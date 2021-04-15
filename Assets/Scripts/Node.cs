d8bbd12cebae9b884e74eb54ac01f869ac177e6c
Yasmin Kruger <mimi_kruger@hotmail.com>
2021-04-15 09:32:10 -0300

    Que funcione dessa vez

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    private Renderer rend;
+    private Color startColor;
+
+    private GameObject turret;
+
+    public Color hoverColor;
+
+    void Start ()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    void OnMouseDown ()
+    {
+        if (turret != null)
+        {
+            Debug.Log("Não é possível construir ai!");
+        }
+    }
+
+    void OnMouserEnter ()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    void OnMouseExit ()
+    {
+        rend.material.color = startColor;
+    }
+}
