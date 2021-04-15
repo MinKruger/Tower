1f657f604c0982429dba060fbf5dcdcf546b9d9b
Yasmin Kruger <mimi_kruger@hotmail.com>
2021-04-15 13:55:41 -0300

    Painel para compra das torres e demais correções

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -4,29 +4,42 @@ using UnityEngine;
 
 public class Node : MonoBehaviour
 {
-    private Renderer rend;
     private Color startColor;
+    public Color hoverColor;
 
+    private Renderer rend;
     private GameObject turret;
 
-    public Color hoverColor;
+    BuildManager buildManager;
 
     void Start ()
     {
         rend = GetComponent<Renderer>();
         startColor = rend.material.color;
+
+        buildManager = BuildManager.instance;
     }
 
     void OnMouseDown ()
     {
+        if (buildManager.GetTurretToBuild() == null)
+            return;
+
         if (turret != null)
         {
             Debug.Log("Não é possível construir ai!");
+            return;
         }
+
+        GameObject turretToBuild = buildManager.GetTurretToBuild();
+        turret = Instantiate(turretToBuild, transform.position, transform.rotation);
     }
 
     void OnMouserEnter ()
     {
+        if (buildManager.GetTurretToBuild() == null)
+            return;
+
         rend.material.color = hoverColor;
     }
 
